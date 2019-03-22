a37e76f9ac301a1ca2829c06215b966fbf8e5aef
Lyndon Brown <jnqnfe@gmail.com>
2019-03-22 04:14:26 +0000

    purge obsolete options removed in v2.0

--- modules/video_output/xcb/x11.c
+++ modules/video_output/xcb/x11.c
@@ -357,6 +357,4 @@ vlc_module_begin()
     set_capability("vout display", 100)
     set_callbacks(Open, Close)
     add_shortcut("xcb-x11", "x11")
-
-    add_obsolete_bool("x11-shm") /* obsoleted since 2.0.0 */
 vlc_module_end()
