cd425f0beafd9c8c9c042ccc25b09d27e7f4f7bd
Lyndon Brown <jnqnfe@gmail.com>
2019-04-02 03:26:42 +0100

    medialibrary: fix double-translation

--- modules/misc/medialibrary/medialib.cpp
+++ modules/misc/medialibrary/medialib.cpp
@@ -1417,8 +1417,8 @@ static void Close( vlc_medialibrary_module_t* module )
     delete p_ml;
 }
 
-#define ML_FOLDER_TEXT _( "Folders discovered by the media library" )
-#define ML_FOLDER_LONGTEXT _( "Semicolon separated list of folders to discover " \
+#define ML_FOLDER_TEXT N_( "Folders discovered by the media library" )
+#define ML_FOLDER_LONGTEXT N_( "Semicolon separated list of folders to discover " \
                               "media from" )
 
 vlc_plugin_begin()
