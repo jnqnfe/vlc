25321b46a067ca8b5cf3082c8b1e0824d3044ee8
Lyndon Brown <jnqnfe@gmail.com>
2019-04-19 09:38:20 +0100

    core/text: malloc caution
    malloc does not necessarily return NULL for size zero

--- src/text/strings.c
+++ src/text/strings.c
@@ -454,7 +454,7 @@ size_t vlc_b64_decode_binary( uint8_t **pp_dst, const char *psz_src )
     const int i_src = strlen( psz_src );
     uint8_t   *p_dst;
 
-    *pp_dst = p_dst = malloc( i_src );
+    *pp_dst = p_dst = malloc( i_src + 1 );
     if( !p_dst )
         return 0;
     return  vlc_b64_decode_binary_to_buffer( p_dst, i_src, psz_src );
