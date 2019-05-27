8664f6127b1912c6cbae5bc47e78ec5af57e68c2
Lyndon Brown <jnqnfe@gmail.com>
2019-05-27 23:25:33 +0100

    macosx: improve key selector item identification
    as just done for Qt (see the explanation there for why checking "key-"
    prefix is a bad idea)

--- modules/gui/macosx/preferences/VLCSimplePrefsController.m
+++ modules/gui/macosx/preferences/VLCSimplePrefsController.m
@@ -825,7 +825,7 @@ static inline const char * __config_GetLabel(vlc_object_t *p_this, const char *p
 
         if (p_item->i_type == CONFIG_ITEM_KEY
            && p_item->psz_name != NULL
-           && !strncmp(p_item->psz_name , "key-", 4)
+           && strncmp(p_item->psz_name , "global-", 7) != 0
            && !EMPTY_STR(p_item->psz_text)) {
             [tempArray_desc addObject: _NS(p_item->psz_text)];
             [tempArray_names addObject: toNSStr(p_item->psz_name)];
