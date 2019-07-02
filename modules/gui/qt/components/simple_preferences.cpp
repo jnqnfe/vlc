jnqnfe/vlc

commit 730c040ef49c30f1957bf7d66848bb779f3b9aa1

vout:win32: remove write-only "overlay" option

modules/gui/qt/components/simple_preferences.cpp
@@ -366,8 +366,6 @@ SPrefsPanel::SPrefsPanel( intf_thread_t *_p_intf, QWidget *_parent,
             CONFIG_BOOL( "video-deco", windowDecorations );
             CONFIG_GENERIC( "vout", StringList, ui.voutLabel, outputModule );
 
-            CONNECT( ui.outputModule, currentIndexChanged( int ),
-                     this, updateVideoOptions( int ) );
             optionWidgets["videoOutCoB"] = ui.outputModule;
 
             optionWidgets["fullscreenScreenB"] = ui.fullscreenScreenBox;
@@ -389,12 +387,7 @@ SPrefsPanel::SPrefsPanel( intf_thread_t *_p_intf, QWidget *_parent,
             }
 
 #ifdef _WIN32
-            CONFIG_BOOL( "directx-overlay", overlay );
             CONFIG_BOOL( "directx-hw-yuv", hwYUVBox );
-            CONNECT( ui.overlay, toggled( bool ), ui.hwYUVBox, setEnabled( bool ) );
-            optionWidgets["directxVideoB"] = ui.directXBox;
-#else
-            ui.directXBox->setVisible( false );
 #endif
 
 #ifdef __OS2__
@@ -416,8 +409,6 @@ SPrefsPanel::SPrefsPanel( intf_thread_t *_p_intf, QWidget *_parent,
                             snapshotsSequentialNumbering );
             CONFIG_GENERIC( "snapshot-format", StringList, ui.arLabel,
                             snapshotsFormat );
-
-            updateVideoOptions( ui.outputModule->currentIndex() );
          END_SPREFS_CAT;
 
         /******************************
@@ -947,17 +938,6 @@ SPrefsPanel::SPrefsPanel( intf_thread_t *_p_intf, QWidget *_parent,
 #undef CONFIG_GENERIC_NO_UI
 #undef CONFIG_GENERIC
 #undef CONFIG_BOOL
-}
-
-void SPrefsPanel::updateVideoOptions( int number )
-{
-    QString value = qobject_cast<QComboBox *>(optionWidgets["videoOutCoB"])
-                                            ->itemData( number ).toString();
-#ifdef _WIN32
-    if( optionWidgets["directxVideoB"] ) {
-        optionWidgets["directxVideoB"]->setVisible( ( value == "directdraw" ) );
-    }
-#endif
 }
 
 

modules/gui/qt/components/simple_preferences.hpp
@@ -116,7 +116,6 @@ private:
 /* Display only the options for the selected audio output */
 private slots:
     void lastfm_Changed( int );
-    void updateVideoOptions( int );
     void updateAudioOptions( int );
     void updateAudioVolume( int );
     void langChanged( int );

modules/gui/qt/ui/sprefs_video.ui
@@ -99,17 +99,10 @@
          <string>DirectX</string>
         </property>
         <layout class="QGridLayout" name="gridLayout_4">
-         <item row="2" column="0">
+         <item row="1" column="0">
           <widget class="QCheckBox" name="hwYUVBox">
            <property name="text">
             <string>Use hardware YUV-&gt;RGB conversions</string>
-           </property>
-          </widget>
-         </item>
-         <item row="0" column="0">
-          <widget class="QCheckBox" name="overlay">
-           <property name="text">
-            <string>Accelerated video output (Overlay)</string>
            </property>
           </widget>
          </item>
@@ -319,7 +312,6 @@
   <tabstop>windowDecorations</tabstop>
   <tabstop>outputModule</tabstop>
   <tabstop>fullscreenScreenBox</tabstop>
-  <tabstop>overlay</tabstop>
   <tabstop>hwYUVBox</tabstop>
   <tabstop>kvaFixT23</tabstop>
   <tabstop>kvaVideoMode</tabstop>

modules/video_output/win32/common.h
@@ -73,8 +73,6 @@ typedef struct vout_display_sys_win32_t
 
     bool use_desktop;     /* show video on desktop window ? */
 
-    bool use_overlay;     /* Are we using an overlay surface */
-
     bool (*pf_GetRect)(const struct vout_display_sys_win32_t *p_sys, RECT *out);
     unsigned int (*pf_GetPictureWidth) (const vout_display_t *);
     unsigned int (*pf_GetPictureHeight)(const vout_display_t *);

modules/video_output/win32/events.c
@@ -63,7 +63,6 @@ struct event_thread_t
 
     /* */
     bool use_desktop;
-    bool use_overlay;
 
     /* Mouse */
     bool is_cursor_hidden;
@@ -441,12 +440,6 @@ void EventThreadUpdateSourceAndPlace( event_thread_t *p_event,
     vlc_mutex_unlock( &p_event->lock );
 }
 
-void EventThreadUseOverlay( event_thread_t *p_event, bool b_used )
-{
-    vlc_mutex_lock( &p_event->lock );
-    p_event->use_overlay = b_used;
-    vlc_mutex_unlock( &p_event->lock );
-}
 bool EventThreadGetAndResetHasMoved( event_thread_t *p_event )
 {
     return atomic_exchange(&p_event->has_moved, false);
@@ -501,7 +494,6 @@ void EventThreadDestroy( event_thread_t *p_event )
 int EventThreadStart( event_thread_t *p_event, event_hwnd_t *p_hwnd, const event_cfg_t *p_cfg )
 {
     p_event->use_desktop = p_cfg->use_desktop;
-    p_event->use_overlay = p_cfg->use_overlay;
     p_event->x           = p_cfg->x;
     p_event->y           = p_cfg->y;
     p_event->width       = p_cfg->width;

modules/video_output/win32/events.h
@@ -31,7 +31,6 @@ typedef struct event_thread_t event_thread_t;
 
 typedef struct {
     bool use_desktop; /* direct3d */
-    bool use_overlay; /* directdraw */
     int x;
     int y;
     unsigned width;
@@ -58,7 +57,6 @@ void            EventThreadUpdateWindowPosition( event_thread_t *, bool *pb_move
 void            EventThreadUpdateSourceAndPlace( event_thread_t *p_event,
                                                  const video_format_t *p_source,
                                                  const vout_display_place_t *p_place );
-void            EventThreadUseOverlay( event_thread_t *, bool b_used );
 bool            EventThreadGetAndResetHasMoved( event_thread_t * );
 
 # ifdef __cplusplus

src/libvlc-module.c
@@ -1619,7 +1619,6 @@ vlc_module_begin ()
               MOUSE_EVENTS_LONGTEXT, true )
     add_obsolete_integer( "vout-event" ) /* deprecated since 1.1.0 */
     add_obsolete_integer( "x11-event" ) /* renamed since 1.0.0 */
-    add_obsolete_bool( "overlay" ) /* renamed since 3.0.0 */
     add_bool( "video-on-top", 0, VIDEO_ON_TOP_TEXT,
               VIDEO_ON_TOP_LONGTEXT, false )
     add_bool( "video-wallpaper", false, WALLPAPER_TEXT,

commit 8e9f900dd97fc7cf5ca9869bf69ddf0d5c584936

qt: fix directx option appearing on non-Windows machines
730c040ef49c30f1957bf7d66848bb779f3b9aa1 mistakenly got rid of the
instruction to hide the groupbox

modules/gui/qt/components/simple_preferences.cpp
@@ -388,6 +388,8 @@ SPrefsPanel::SPrefsPanel( intf_thread_t *_p_intf, QWidget *_parent,
 
 #ifdef _WIN32
             CONFIG_BOOL( "directx-hw-yuv", hwYUVBox );
+#else
+            ui.directXBox->setVisible( false );
 #endif
 
 #ifdef __OS2__